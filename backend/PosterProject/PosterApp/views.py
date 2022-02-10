7eab076a074a659c303b07828bb7716c03b66714
Artem <Luzifer_Morningstar@mail.ru>
2022-02-10 18:12:30 +0300

    created CRUD API for User

--- backend/PosterProject/PosterApp/views.py
+++ backend/PosterProject/PosterApp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+# User views
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
+
+	def get(self, request, id):
+		user = User.objects.get(pk=id)
+		serializer = UserSerializer(user)
+		return Response(serializer.data)
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
+
+
+# Post views
+
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
