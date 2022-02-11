4a871f11922b97ea11482e623906dcd931951280
Artem <Luzifer_Morningstar@mail.ru>
2022-02-11 19:02:48 +0300

    created CRUD API for Post and Platform

--- backend/PosterProject/PosterApp/views.py
+++ backend/PosterProject/PosterApp/views.py
@@ -1,6 +1,7 @@
 from .serializers import *
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import authentication
 
 # User views
 
@@ -14,6 +15,7 @@ class UserListAPIView(generics.ListAPIView):
 class UserRetrieveAPIView(generics.RetrieveAPIView):
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
+	authentication_classes = [authentication.TokenAuthentication]
 
 	def get(self, request, id):
 		user = User.objects.get(pk=id)
@@ -22,6 +24,11 @@ class UserRetrieveAPIView(generics.RetrieveAPIView):
 
 
 class UserCreateAPIView(generics.CreateAPIView):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
 
@@ -38,3 +45,56 @@ class PostListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         return Post.objects.all()
+
+
+class PostRetrieveAPIView(generics.RetrieveAPIView):
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
+
+	def get(self, request, id):
+		post = Post.objects.get(pk=id)
+		serializer = PostSerializer(post)
+		return Response(serializer.data)
+
+
+class PostCreateAPIView(generics.CreateAPIView):
+	serializer_class = PostSerializer
+
+
+class PostUpdateAPIView(generics.UpdateAPIView):
+	serializer_class = PostSerializer
+
+
+class PostDestroyAPIView(generics.DestroyAPIView):
+	serializer_class = PostSerializer
+
+
+# Platform views
+
+class PlatformListAPIView(generics.ListAPIView):
+    serializer_class = PlatformSerializer
+
+    def get_queryset(self):
+        return Platform.objects.all()
+
+
+class PlatformRetrieveAPIView(generics.RetrieveAPIView):
+	serializer_class = PlatformSerializer
+	queryset = Platform.objects.all()
+	
+	def get(self, request, id):
+		post = Platform.objects.get(pk=id)
+		serializer = PlatformSerializer(post)
+		return Response(serializer.data)
+
+
+class PlatformCreateAPIView(generics.CreateAPIView):
+	serializer_class = PlatformSerializer
+
+
+class PlatformUpdateAPIView(generics.UpdateAPIView):
+	serializer_class = PlatformSerializer
+
+
+class PlatformDestroyAPIView(generics.DestroyAPIView):
+	serializer_class = PlatformSerializer
