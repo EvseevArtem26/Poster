8cae1818ab77edeea01acaf1d38d1afba55c10d8
Artem <Luzifer_Morningstar@mail.ru>
2022-03-02 20:38:28 +0300

    Added Navbar

--- frontend/poster/src/App.vue
+++ frontend/poster/src/App.vue
@@ -1,32 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- <navigation-bar class="mx-auto" outlined/> -->
+    <v-toolbar
+    color="purple"
+    dark
+  >
+    <v-toolbar-title>Title</v-toolbar-title>
+
+    <v-divider
+      class="mx-4"
+      vertical
+    ></v-divider>
+
+    <span class="subheading">My Home</span>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text>
+        News
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>
+        Blog
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>
+        Music
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+    </v-toolbar-items>
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </v-toolbar>
   </div>
 </template>
 
+<script>
+// import NavigationBar from '@/components/NavigationBar.vue'
+export default {
+  // components: { NavigationBar }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ frontend/poster/src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-toolbar
+  color="green"
+  class="smAndUp-12"
+  min-width="1000px"
+  outlined
+  >
+
+  <span class="subheading">My Home</span>
+
+  <v-toolbar-items class="hidden-sm-and-down">
+    <v-btn text>
+    News
+    </v-btn>
+
+    <v-divider
+    class="lg-4"
+    vertical>
+    </v-divider>
+
+    <v-btn text>
+    Blog
+    </v-btn>
+
+    <v-divider vertical></v-divider>
+
+    <v-btn text>
+    Music
+    </v-btn>
+
+  </v-toolbar-items>
+
+  </v-toolbar>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
