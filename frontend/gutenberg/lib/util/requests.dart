2d056805fad6cc25a986eba7bce76ca1c8ea4bc3
Artem <Luzifer_Morningstar@mail.ru>
2022-04-24 20:35:31 +0300

    Removed unused functions

--- frontend/gutenberg/lib/util/requests.dart
+++ frontend/gutenberg/lib/util/requests.dart
@@ -1,10 +1,12 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/post.dart';
 
-Future<http.Response> savePost(Post post) async {
+Future<void> savePost(Post post) async {
   
-  var request = post.toJson();
+  final prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token') ?? '';
 
   Uri url = Uri(
     scheme: "http",
@@ -12,76 +14,23 @@ Future<http.Response> savePost(Post post) async {
     port: 8000,
     path: "/poster/posts/"
   );
-  Map body = {
-    "title":"Testing post saving",
-    "text":"Hello, django",
-    "publication_time": DateTime.now().toIso8601String(),
-    "author":"1"
-  };
+  String json = jsonEncode(post.toJson());
+  print(json);
   var response = await http.post(
     url, 
-    body: jsonEncode(body),
+    body: json,
     headers: {
-      'Accept': 'application/json',
-      "Content-Type":"application/json; charset=UTF-8"
+      "content-type": "application/json",
+      "accept": "application/json",
+      "Authorization": "Token $token"
     }
   );
-  
-  return response;
+  if(response.statusCode == 201){      
+      print('Post saved');
+    }
+    else {
+      print('Failed to create post\ncode: ${response.statusCode}');
+      print(response.body);
+    }
 }
 
-void testSavePost()async{
-  Post testPost = Post(
-    title: "Testing post saving",
-    text: "Hello, django",
-    publicationTime: DateTime.now(),
-    author: 1,
-    platforms: [1]
-  );
-  http.Response response = await savePost(testPost);
-  print(response.body);
-}
-
-
-Future<http.Response> getPosts()async{
-  Uri url = Uri(
-    scheme: "http",
-    host: "localhost",
-    port: 8000,
-    path: "/poster/posts/"
-  );
-  http.Response response = await http.get(url);
-  return response;
-}
-
-void testGetPosts()async{
-  http.Response data = await getPosts();
-  print(data.body);
-}
-
-Future<http.Response> savePlatform()async{
-  Uri url = Uri(
-    scheme: "http",
-    host: "localhost",
-    port: 8000,
-    path: "/poster/platforms/"
-  );
-  Map<String, String> body = {
-    "login":"test",
-    "password":"test",
-    "email":"test@mail.ru",
-    "phone_number":"111111111",
-    "platform":"TW",
-    "user":"1"
-  };
-  http.Response response = await http.post(
-    url,
-    body: body
-  );
-  return response;
-}
-
-void testSavePlatform()async{
-  http.Response response = await savePlatform();
-  print(response.body);
-}
