594bdb1a0c92ac649aa70eb50590614059493761
Artem <Luzifer_Morningstar@mail.ru>
2022-06-06 02:37:16 +0300

    Heavily optimized publishing speed and reliability

--- backend/PosterProject/PosterApp/api_calls.py
+++ backend/PosterProject/PosterApp/api_calls.py
@@ -1,11 +1,13 @@
 import magic 
 from selenium import webdriver
+import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.proxy import *
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException
 from py3pin.Pinterest import Pinterest
 
 import time
@@ -64,154 +66,222 @@ def activate_vpn(driver):
 def send_post(post):
     # send post to platform
     if post.platform.platform == 'OK':
-        driver = initialize_driver(headless=True)
+        driver = initialize_driver(headless=False)
         send_post_to_odnoklassniki(driver, post)
-        driver.quit()
     if post.platform.platform == 'VK':
         driver = initialize_driver(headless=True)
         send_post_to_vkontakte(driver, post)
-        driver.quit()
     if post.platform.platform == 'TW':
-        driver = initialize_driver(headless=False, vpn=True)
+        driver = initialize_driver(headless=True, vpn=False)
         send_post_to_twitter(driver, post)
-        driver.quit()
     if post.platform.platform == "PT":
         send_post_to_pinterest(post)
 
 @time_this
 def send_post_to_odnoklassniki(driver, post):
+    url = 'https://ok.ru/'
+    login_field_xpath = "//*[@id='field_email']"
+    password_field_xpath = "//*[@id='field_password']"
+    login_button_xpath = "//*[@class='login-form-actions']/input"
+    post_form_classname = "pf-head_itx_a"
+    text_field_xpath = "//*[@class='posting_itx-w']/div[2]"
+    add_image_button_xpath = "//*[@data-l='t,button.photo']"
+    image_upload_field_xpath = "//*[@id='hook_Block_PopLayer']/div/photo-picker/div/div/div[2]/div/div[3]/div[2]/div[1]/span/input"
+    add_video_button_xpath = "//*[@data-l='t,button.video']"
+    video_upload_field_xpath = "/html/body/div[17]/div/div[2]/div[1]/div/div[3]/div/div[1]/div/div/div[2]/span/input"
+    submit_button_xpath = "//*[@class='posting_f_ac']/div[2]"
+    
+
     phone = post.platform.phone_number
     password = post.platform.password
-    driver.get("https://ok.ru/")
-    time.sleep(5)
-    login_field = driver.find_element(by=By.XPATH, value="//*[@id='field_email']")
-    login_field.send_keys(phone)
-    time.sleep(1)
-    password_field = driver.find_element(by=By.XPATH, value="//*[@id='field_password']")
-    password_field.send_keys(password)
-    time.sleep(1)
-    driver.find_element(by=By.XPATH, value="//*[@class='login-form-actions']/input").click()
-    time.sleep(5)
-    driver.find_element(by=By.CLASS_NAME, value="pf-head_itx_a").click()
-    time.sleep(3)
-    input = driver.find_element(by=By.XPATH, value="//*[@class='posting_itx-w']/div[2]")
-    input.send_keys(post.text)
-    time.sleep(3)
-    submit_button = driver.find_element(by=By.XPATH, value="//*[@class='posting_f_ac']/div[2]")
 
-    if post.media:
-        if get_mime_type(post.media) == 'image':
-            driver.find_element(by=By.XPATH, value="//*[@data-l='t,button.photo']").click()
-            time.sleep(3)
-            driver.find_element(by=By.XPATH, value="//*[@id='hook_Block_PopLayer']/div/photo-picker/div/div/div[2]/div/div[3]/div[2]/div[1]/span/input").send_keys(post.media.path)
-            time.sleep(3)
-        if get_mime_type(post.media) == 'video':
-            # driver.find_element(by=By.XPATH, value="//*[@data-l='t,button.video']").click()
-            # time.sleep(3)
-            # driver.find_element(by=By.XPATH, value="//*[@id='hook_Block_PFVideoAttachUploadBlock']/div/div/div[2]/span/input").send_keys(post.media.path)
-            # wait until submit button is enabled
-            # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@class='posting_f_ac']/div[2]")))
-            time.sleep(3)
-            # time.sleep(10)
-    submit_button.click()
-    post.status = 'published'
-    post.save()
+    if phone == '' or password == '':
+        return
+
+    try:
+        driver.get(url)
+
+        login_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=login_field_xpath))
+        login_field.send_keys(phone)
+
+        password_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=password_field_xpath))
+        password_field.send_keys(password)
+
+        login_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=login_button_xpath))
+        login_button.click()
+
+        post_form = WebDriverWait(driver, 20).until(lambda driver: driver.find_element(by=By.CLASS_NAME, value=post_form_classname))
+        post_form.click()
+
+        text_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=text_field_xpath))
+        text_field.clear()
+        text_field.send_keys(post.text)
+
+        
+
+        if post.media:
+            if get_mime_type(post.media) == 'image':
+                add_image_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=add_image_button_xpath))
+                add_image_button.click()
+                
+                image_upload_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=image_upload_field_xpath))
+                image_upload_field.send_keys(post.media.path)
+                WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, submit_button_xpath)))
+
+                # WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value="//*[@id='hook_Block_pf2']/div[3]/div/ul/li/div[3]"))
+                
+            if get_mime_type(post.media) == 'video':
+                add_video_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=add_video_button_xpath))
+                add_video_button.click()
+                
+                video_upload_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=video_upload_field_xpath))
+                print(post.media.path)
+                video_upload_field.send_keys(post.media.path)
+                time.sleep(30)
+                # close_button = driver.find_element(by=By.XPATH, value="//*[@id='nohook_modal_close']")
+                # close_button.click()
+             
+                # action = ActionChains(driver)
+                # action.send_keys(Keys.ESCAPE)
+                # action.perform()
+                # time.sleep(5)
+                # WebDriverWait(driver, 60,poll_frequency=5 ,ignored_exceptions=[NoSuchElementException, ElementClickInterceptedException]).until(EC.element_to_be_clickable((By.XPATH, submit_button_xpath)))
+                WebDriverWait(driver, 60).until(lambda driver: driver.find_element(by=By.XPATH, value="/html/body/div[9]/div/div[1]/div[2]/div/div/div[1]/div[1]/div[2]/div/div[5]/div[2]/div[4]/div/div/div[2]"))
+
+        submit_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=submit_button_xpath))
+        submit_button.click()
+
+        post.status = 'published'
+        post.save() 
+
+    except Exception as e:
+        print(e)
+        post.status = 'delayed'
+        post.save()
+    finally:
+        driver.quit()
 
 @time_this
 def send_post_to_vkontakte(driver, post):
+    url = "https://vk.com/"
+    login_button_xpath = "//*[@id='index_login']/div/form/button"
+    login_field_xpath = "//*[@name='login']"
+    password_field_xpath = "//*[@name='password']"
+    post_form_xpath = "//*[@id='post_field']"
+    add_image_button_xpath = "//*[@id='page_add_media']/div[1]/a[1]"
+    image_upload_field_xpath = "//*[@id='photos_upload_input_729012740_-14']"
+    add_video_button_xpath = "//*[@id='page_add_media']/div[1]/a[2]"
+    video_upload_field_xpath = "//*[@id='choose_video_upload']/input"
+    send_post_button_xpath = "//*[@id='send_post']"
+
     phone = post.platform.phone_number
     password = post.platform.password
-    driver.get("https://vk.com/")
-    time.sleep(5)
-    driver.find_element(by=By.XPATH, value="//*[@id='index_login']/div/form/button").click()
-    time.sleep(3)
-    login_field = driver.find_element(by=By.XPATH, value="//*[@name='login']")
-    login_field.send_keys(phone)
-    login_field.send_keys(Keys.ENTER)
-    time.sleep(2)
-    password_field = driver.find_element(by=By.XPATH, value="//*[@name='password']")
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.ENTER)
-    time.sleep(5)
-    input = driver.find_element(by=By.XPATH, value="//*[@id='post_field']")
-    input.send_keys(post.text)
-    if post.media:
-        if get_mime_type(post.media) == 'image':
-            driver.find_element(by=By.XPATH, value="//*[@id='page_add_media']/div[1]/a[1]").click()
-            time.sleep(3)
-            driver.find_element(by=By.XPATH, value="//*[@id='photos_upload_input_729012740_-14']").send_keys(post.media.path)
-        if get_mime_type(post.media) == 'video':
-            driver.find_element(by=By.XPATH, value="//*[@id='page_add_media']/div[1]/a[2]").click()
-            time.sleep(3)
-            driver.find_element(by=By.XPATH, value="//*[@id='choose_video_upload']/input").send_keys(post.media.path)
-        time.sleep(5)
+    if phone == '' or password == '':
+        return
+        
+    try:
+        driver.get(url)
 
+        login_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=login_button_xpath))
+        login_button.click()
 
-    action = ActionChains(driver)
-    action.key_down(Keys.CONTROL)
-    action.send_keys(Keys.ENTER)
-    action.key_up(Keys.CONTROL)
-    action.perform()
-    time.sleep(1)
-    post.status = 'published'
-    post.save()
+        login_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=login_field_xpath))
+        login_field.send_keys(phone)
+        login_field.send_keys(Keys.ENTER)
+
+        password_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=password_field_xpath))
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.ENTER)
+
+        input = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=post_form_xpath))
+        input.clear()
+        input.send_keys(post.text)
+        if post.media:
+            if get_mime_type(post.media) == 'image':
+                add_image_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=add_image_button_xpath))
+                add_image_button.click()
+                image_upload_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=image_upload_field_xpath))
+                image_upload_field.send_keys(post.media.path)
+                WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value="//*[@id='thumbs_edit1']/div/div[1]/div[3]"))
+                
+            if get_mime_type(post.media) == 'video':
+                add_video_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=add_video_button_xpath))
+                add_video_button.click()
+                video_upload_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=video_upload_field_xpath))
+                video_upload_field.send_keys(post.media.path)
+                WebDriverWait(driver, 60).until(lambda driver: driver.find_element(by=By.XPATH, value="//*[@id='thumbs_edit1']/div/div[1]/div"))
+
+        send_post_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=send_post_button_xpath))
+        send_post_button.click()
+
+        post.status = 'published'
+        post.save()
+    except Exception as e:
+        print(e)
+        post.status = 'delayed'
+        post.save()
+    finally:
+        driver.quit()    
 
 
 @time_this
 def send_post_to_twitter(driver, post):
-    activate_vpn(driver)
+    url = "https://twitter.com/i/flow/login"
+    tweet_url = "https://twitter.com/compose/tweet"
+    login_field_xpath = "//*[@id='react-root']/div/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
+    phone_field_xpath = "//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input"
+    password_field_xpath = "//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
+    input_field_xpath = "//*[@class='DraftEditor-editorContainer']/div"
+    file_field_xpath = "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[1]/input"
+    submit_button_xpath = "//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]"
+
     email = post.platform.email
     password = post.platform.password
-    driver.get('https://twitter.com/i/flow/login')
-    time.sleep(10)
-    login_field = driver.find_element(by=By.XPATH, value="//*[@id='react-root']/div/div/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")
-    time.sleep(1)
+    phone = post.platform.phone_number
+    # activate_vpn(driver)
+    driver.get(url)
+
+    login_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=login_field_xpath))
     login_field.send_keys(email)
     login_field.send_keys(Keys.ENTER)
-    time.sleep(5)
-    password_field = driver.find_element(by=By.NAME, value="password")
-    time.sleep(1)
+    
+    
+    try:
+        phone_field = WebDriverWait(driver, 10).until(lambda driver: driver.find_element(by=By.XPATH, value=phone_field_xpath))
+        phone_field.send_keys(phone, Keys.ENTER)
+    except TimeoutException:
+        pass
+
+
+    password_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.NAME, value="password"))
     password_field.send_keys(password)
     password_field.send_keys(Keys.ENTER)
-    time.sleep(5)
-    driver.get('https://twitter.com/compose/tweet')
-    time.sleep(10)
-    input_field = driver.find_element(by=By.XPATH, value="//*[@class='DraftEditor-editorContainer']/div")
-    time.sleep(5)
-    input_field.send_keys(post.text)
-    print(post.media.path if post.media else '')
-    if post.media and get_mime_type(post.media) == 'image':
-        driver.find_element(by=By.XPATH,
-        value="//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[1]/input").send_keys(post.media.path)
-        time.sleep(15)
 
-    action = ActionChains(driver)
-    action.key_down(Keys.CONTROL)
-    action.send_keys(Keys.ENTER)
-    action.key_up(Keys.CONTROL)
-    action.perform()
-    time.sleep(5)
+    # driver.get(tweet_url)
+    tweet_button = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value="//*[@id='react-root']/div/div/div[2]/header/div/div/div/div[1]/div[3]/a"))
+    tweet_button.click()
+
+    input_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=input_field_xpath))
+    input_field.send_keys(post.text)
+
+    if post.media:
+        if get_mime_type(post.media) == 'image':
+            file_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=file_field_xpath))
+            file_field.send_keys(post.media.path)
+            
+        if get_mime_type(post.media) == 'video':
+            file_field = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=file_field_xpath))
+            file_field.send_keys(post.media.path)
+    print(driver.title)
+    submit_button_xpath = WebDriverWait(driver, 15).until(lambda driver: driver.find_element(by=By.XPATH, value=submit_button_xpath))
+    submit_button_xpath.click()
+    
+    WebDriverWait(driver, 60).until_not(EC.url_to_be(tweet_url))
+
     post.status = 'published'
     post.save()
 
-@time_this
-def send_post_to_facebook(driver, post):
-    activate_vpn(driver)
-    phone = post.platform.phone_number
-    password = post.platform.password
-    driver.get('https://www.facebook.com/')
-    driver.find_element(by=By.ID, value="email").send_keys(phone)
-    time.sleep(3)
-    driver.find_element(by=By.ID, value="pass").send_keys(password, Keys.ENTER)
-    time.sleep(8)
-    driver.get('https://www.facebook.com/profile.php')
-
-    driver.find_element(by=By.XPATH, value="//*[@data-pagelet='ProfileComposer']/div/div/div/div/div[1]/div").click()
-    time.sleep(10)
-    driver.find_element(by=By.XPATH, value="//*[@role='presentation']/div[1]/div/div/div").send_keys(post.text)
-    time.sleep(5)
-    driver.find_element(by=By.XPATH, value="//*[@aria-label='Опубликовать']").click()
-    time.sleep(5)
+    driver.quit()
 
 @time_this
 def send_post_to_pinterest(post):
